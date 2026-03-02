492300ca347c070c562dd3fab158746d1c746455
karalarmehmet <mehmetkaralarm@gmail.com>
2026-03-02 23:02:38 +0300

    Fix cursor_factory re-wrapping with connection pooling

--- django_prometheus/db/backends/postgis/base.py
+++ django_prometheus/db/backends/postgis/base.py
@@ -1,21 +1,16 @@
 from django.contrib.gis.db.backends.postgis import base
-from django.db.backends.postgresql.base import Cursor
 
 from django_prometheus.db.common import DatabaseWrapperMixin, ExportingCursorWrapper
 
 
 class DatabaseWrapper(DatabaseWrapperMixin, base.DatabaseWrapper):
-    def get_new_connection(self, *args, **kwargs):
-        conn = super().get_new_connection(*args, **kwargs)
-        conn.cursor_factory = ExportingCursorWrapper(
-            conn.cursor_factory or Cursor(),
-            "postgis",
-            self.vendor,
-        )
-
-        return conn
+    def get_connection_params(self):
+        conn_params = super().get_connection_params()
+        conn_params["cursor_factory"] = ExportingCursorWrapper(conn_params["cursor_factory"], self.alias, self.vendor)
+        return conn_params
 
     def create_cursor(self, name=None):
-        # cursor_factory is a kwarg to connect() so restore create_cursor()'s
-        # default behavior
+        # cursor_factory is set in get_connection_params() so psycopg already
+        # creates instrumented cursors. Delegate to Django's implementation to
+        # avoid the mixin's create_cursor which is incompatible with psycopg3.
         return base.DatabaseWrapper.create_cursor(self, name=name)
